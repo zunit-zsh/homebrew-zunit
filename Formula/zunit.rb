47ff0b4ebd9c7055e1dfd3d17e65c1f7665766bb
James Dinsdale <hi@molovo.co>
2017-07-10 22:16:21 +0100

    Bump version to 0.8.0

--- Formula/zunit.rb
+++ Formula/zunit.rb
@@ -1,8 +1,8 @@
 class Zunit < Formula
   desc "Powerful testing framework for ZSH projects"
   homepage "https://zunit.xyz"
-  url "https://github.com/zunit-zsh/zunit/archive/v0.7.0.tar.gz"
-  sha256 "4833b869baad8d396d5436faa0ea86b2caec8bbb0022495c5c17957ce4eb6ff1"
+  url "https://github.com/zunit-zsh/zunit/archive/v0.8.0.tar.gz"
+  sha256 "f4fcdc038e890673b9c793ade7fb2a5965ce618c51935f84021af6a413a6f602"
   head "https://github.com/zunit-zsh/zunit.git", :branch => "next"
 
   devel do
@@ -14,18 +14,7 @@ class Zunit < Formula
   bottle :unneeded
   depends_on "molovo/revolver/revolver" => :run
 
-  resource "color" do
-    url "https://github.com/molovo/color.git"
-    sha256 "9b6787e3503d4923d6aafa1b52f62efce68000cfdd8d52b313ebc36cb40de2a2"
-  end
-
   def install
-    resource("color").stage do
-      mv "color.zsh", "color"
-      lib.install "color"
-      bin.install_symlink "#{lib}/color"
-    end
-
     system "./build.zsh"
     bin.install "zunit"
     zsh_completion.install "zunit.zsh-completion" => "_zunit"
@@ -34,4 +23,4 @@ class Zunit < Formula
   test do
     assert_equal version.to_s, shell_output("#{bin}/zunit -v")
   end
- end
+end
