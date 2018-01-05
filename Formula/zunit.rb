0a6dea6e9a783bc01df59936bb7f60e0c35ea7d9
James Dinsdale <hi@molovo.co>
2018-01-05 00:00:21 +0000

    Bump version

--- Formula/zunit.rb
+++ Formula/zunit.rb
@@ -1,14 +1,14 @@
 class Zunit < Formula
   desc "Powerful testing framework for ZSH projects"
   homepage "https://zunit.xyz"
-  url "https://github.com/zunit-zsh/zunit/archive/v0.8.1.tar.gz"
-  sha256 "5a8e39143b294df67d923814221676ca5a58e36295c458486096e56f94c031a9"
+  url "https://github.com/zunit-zsh/zunit/archive/v0.8.2.tar.gz"
+  sha256 "eb94dad722dc7e95395f6e4b6a3e2eea5f7fd487d807112582a73ed7c3b2e4a7"
   head "https://github.com/zunit-zsh/zunit.git", :branch => "next"
 
   devel do
-    url "https://github.com/zunit-zsh/zunit/archive/v0.8.1-beta.tar.gz"
-    version "0.8.1-beta"
-    sha256 "e2af97277664c755268ddb8d4e6c1b7eac009760bbf15a430a11ac90f53f3c6d"
+    url "https://github.com/zunit-zsh/zunit/archive/v0.8.2.tar.gz"
+    version "0.8.2"
+    sha256 "eb94dad722dc7e95395f6e4b6a3e2eea5f7fd487d807112582a73ed7c3b2e4a7"
   end
 
   bottle :unneeded
