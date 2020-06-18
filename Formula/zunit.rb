5445a0d134a93b67298cd085f47662b64e3719a7
samhstn <shouston3@me.com>
2020-06-18 18:28:20 +0100

    Change 'devel' to 'head' as 'devel' syntax is deprecated. Fixes #2

--- Formula/zunit.rb
+++ Formula/zunit.rb
@@ -5,7 +5,7 @@ class Zunit < Formula
   sha256 "eb94dad722dc7e95395f6e4b6a3e2eea5f7fd487d807112582a73ed7c3b2e4a7"
   head "https://github.com/zunit-zsh/zunit.git", :branch => "next"
 
-  devel do
+  head do
     url "https://github.com/zunit-zsh/zunit/archive/v0.8.2.tar.gz"
     version "0.8.2"
     sha256 "eb94dad722dc7e95395f6e4b6a3e2eea5f7fd487d807112582a73ed7c3b2e4a7"
